1dc21dfa33922a56143f825520ae75693a08d55c
Victormg42 <100005779.joan23@fje.edu>
2020-11-30 20:28:46 +0100

    Tercer commit

---
+++ controller/postController.php
@@ -0,0 +1,15 @@
+<?php
+require_once '../model/connection.php';
+$title = $_POST['title'];
+$path = 'public/'.$_FILES['img']['name'];
+/* el ID del user se ha de colocar de manera correcta y no de manera hardcodeada */
+$query = "INSERT INTO posts (title, path, user) VALUES(?,?,'1')";
+$sentencia = $pdo->prepare($query);
+$sentencia->bindParam(1,$title);
+$sentencia->bindParam(2,$path);
+$sentencia->execute();
+
+move_uploaded_file($_FILES['img']['tmp_name'], '../'.$path);
+
+//header("Location: ../view/home.html");
+?>

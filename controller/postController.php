df0766a9c3831a6efbf09c7651818cb706b4e4d6
Victormg42 <100005779.joan23@fje.edu>
2020-11-30 20:46:38 +0100

    Cuarto commit

--- controller/postController.php
+++ controller/postController.php
@@ -2,14 +2,12 @@
 require_once '../model/connection.php';
 $title = $_POST['title'];
 $path = 'public/'.$_FILES['img']['name'];
-/* el ID del user se ha de colocar de manera correcta y no de manera hardcodeada */
-$query = "INSERT INTO posts (title, path, user) VALUES(?,?,'1')";
-$sentencia = $pdo->prepare($query);
-$sentencia->bindParam(1,$title);
-$sentencia->bindParam(2,$path);
-$sentencia->execute();
-
-move_uploaded_file($_FILES['img']['tmp_name'], '../'.$path);
-
-//header("Location: ../view/home.html");
-?>
+if (move_uploaded_file($_FILES['img']['tmp_name'], '../'.$path)) {
+    /* el ID del user se ha de colocar de manera correcta y no de manera hardcodeada */
+    $query = "INSERT INTO posts (title, path, user) VALUES(?,?,'1')";
+    $sentencia = $pdo->prepare($query);
+    $sentencia->bindParam(1,$title);
+    $sentencia->bindParam(2,$path);
+    $sentencia->execute();
+    header("Location: ../view/home.html");
+}
